bb4ba8019abdcb5252962b84f20bd16596a8b98f
sergio parra <sergiolparra666@gmail.com>
2021-09-29 18:19:08 -0400

    vista de resgistro terminada

--- UEBJuan_Ignacio_Montilla/settings.py
+++ UEBJuan_Ignacio_Montilla/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'UEBJuan_Ignacio_Montilla.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':'institucion',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': '127.0.0.1',
-#         'DATABASE_PORT': '5432',
-        
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'institucion',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
+        
+    }
+}
 
 
 # Password validation
